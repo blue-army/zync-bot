e46e7b664e0da361a3044018736567cfec32ae87
rbauer <rebeccabauer@users.noreply.github.com>
2017-06-29 15:52:23 -0700

    changed text board style

--- messages/text_board.js
+++ messages/text_board.js
@@ -1,17 +1,28 @@
 // var sp = require('sprintf-js').vsprintf;
 var vsprintf = require('sprintf-js').vsprintf;
 
+var style1 = {
+   "header": "~ | %s | %s | %s | <br/>",
+   "row"   : "%s | %s | %s | %s | <br/>"
+}
+
+var style2 = {
+   "header": "~~ %s, %s, %s ~~ <br/>",
+   "row"   : "%s [ %s, %s, %s ] <br/>"
+}
+
+var style = style2;
 var colNames = ['A', 'B', 'C'];
 var rowNames = ['1', '2', '3'];
 var chars = ['_', 'X', 'O'];
 function TextBoard(board_state) {
-  var board = vsprintf("~ | %s | %s | %s | <br/>", colNames);
+  var board = vsprintf(style.header, colNames);
   for (var row = 0; row < 3; row ++) {
      tokens = [rowNames[row]];
      for (var col = 0; col < 3; col ++) {
         tokens.push(chars[board_state[row][col]]);
      }
-     board += vsprintf("%s | %s | %s | %s | <br/>", tokens);
+     board += vsprintf(style.row, tokens);
  }
  return board;
 }
